6ed3e7931db89dc3c141b8b06222863ad0d6b337
anhquanqueo25 <aqdz01@gmail.com>
2021-10-27 20:36:22 +0700

    Ho Hong Quan

---
+++ admin/del-teacher-admin.php
@@ -0,0 +1,17 @@
+<?php session_start() ?>
+<?php include('../config/config.php') ?>
+<?php
+    if(isset($_GET['idGV'])){
+        $idGV = $_GET['idGV'];
+        $sql = "DELETE FROM giaovien WHERE idGV = '$idGV'";
+        $res = mysqli_query($conn, $sql);
+        if($res == true){
+            header('location: teacher.php');
+            $_SESSION['successDel'] = "<div class='success'>Xóa thành công!</div>";
+        }
+        else{
+            header('location:teacher.php');
+            $_SESSION['errorDel'] = "<div class='error'>Giáo viên hiện đang có trong danh sách dạy, không thể xóa!</div>";
+        }
+    }
+?>
