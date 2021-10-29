8cec13f537f1fbaab568e67d54ce82e0db7a8fb8
minhvuong25 <90454436+minhvuong25@users.noreply.github.com>
2021-10-30 02:06:35 +0700

    nguyen minh vuong

--- admin/student/dsnhom-btl-admin.php
+++ admin/student/dsnhom-btl-admin.php
@@ -1,16 +1,13 @@
-<?php
-session_start();
-?>
-<title>STUDENT</title>
+<title>TEAM LIST</title>
 <?php include('../../config/config.php');
-
+session_start();
 if (!isset($_SESSION['student'])) {
     header('location:../../login/index.php');
 }
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="../../css/teacher-admin.css">
+<link rel="stylesheet" href="../../css/list-dk-btl.css">
 <?php
 if (isset($_GET['idBTL'])) {
     $idBTL = $_GET['idBTL'];
@@ -19,11 +16,6 @@ if (isset($_GET['idBTL'])) {
     $res1 = mysqli_query($conn, $sql1);
     $row1 = mysqli_fetch_assoc($res1);
     $nameBTL = $row1['nameBTL'];
-
-    $sql0 = "SELECT * FROM btl WHERE idBTL = $idBTL ";
-    $res0= mysqli_query($conn, $sql0);
-    $row0 = mysqli_fetch_assoc($res0);
-    $note = $row0['note'];
 }
 ?>
 <div id="wrapper">
@@ -35,10 +27,23 @@ if (isset($_GET['idBTL'])) {
                 <h2>STUDENT</h2>
             </li>
             <li>
+                <a href="index.php"><i class="fas fa-chart-line"></i> Dashboard</a>
+            </li>
+            <li>
+                <a href="teacher.php"><i class="fas fa-chalkboard-teacher"></i> Teacher</a>
+            </li>
+            <li>
                 <a href="student.php"><i class="fas fa-user-graduate"></i> Student</a>
             </li>
             <li>
                 <a href="subject.php"><i class="fas fa-book"></i> Subject</a>
+            </li>
+            <li>
+                <a href="teach.php"><i class="fas fa-school"></i> Teach</a>
+            </li>
+
+            <li>
+                <a href="result-admin.php"><i class="fas fa-poll"></i> Result</a>
             </li>
         </ul>
     </div>
@@ -49,93 +54,77 @@ if (isset($_GET['idBTL'])) {
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
+
                     <nav class="navbar navbar-light">
                         <div class="container-fluid">
                             <a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><i class="fas fa-bars"></i></a>
 
                             <form class="d-flex">
                                 <a href="" class="navbar-brand">HOME</a>
-                                <a href="./myprofile.php" class="navbar-brand">ACCOUNT</a>
+                                <a href="" class="navbar-brand">ACCOUNT</a>
                                 <a href="../../login/logout.php" class="navbar-brand">LOGOUT</a>
                                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                                 <button class="btn btn-outline-success" type="submit">Search</button>
                             </form>
                         </div>
                     </nav>
-                    <div class="container">
-                        <br>
-                        <div class="header-page">
-                            <h1 class="page-title">STUDENT</h1>
-                            <!--  -->
+                    <div class="main-content">
+
+
+                        <div class="container">
+                            <br>
+
+                            <div class="tittle-mh">
+                                <h1><?php echo $nameBTL ?></h1>
+                            </div>
                             <?php
-                            if (isset($_SESSION['display-username'])) {
-                                echo $_SESSION['display-username'];
+                            $sql3 = "SELECT * FROM btlsv WHERE idBTL = $idBTL";
+                            $sql4 = "SELECT * FROM btlsv WHERE idBTL = $idBTL";
+                            $res4 = mysqli_query($conn,$sql4);
+                            $res3 = mysqli_query($conn, $sql3);
+                            $row4 = mysqli_fetch_assoc($res4)
+                            ?>
+                            <?php
+
+                            if ($res3 == true) {
+                                
+                            ?>
+                                <h2 style="color:darkgrey"><?php echo $row4['note'] ?></h2>
+                                <?php
+                                $i = 1;
+                                while ($row3 = mysqli_fetch_assoc($res3)) {  
+                                ?>
+                                
+                                    <p><span style="font-weight: 500;">Thành viên <?php echo $i ?>: </span><?php echo $row3['user_id'] ?><span style="font-weight: 500;"> (Trưởng nhóm)</span></p>
+                                <?php
+                                    $i++;
+                                } ?>
+                                <p class="note"><?php echo $row1['nameBTL'] ?></p>
+                            <?php
                             }
                             ?>
-                        </div>
 
-                        <?php
-                        if (isset($_SESSION['errorDel'])) {
-                            echo $_SESSION['errorDel'];
-                            unset($_SESSION['errorDel']);
-                        }
-                        if (isset($_SESSION['successDel'])) {
-                            echo $_SESSION['successDel'];
-                            unset($_SESSION['successDel']);
-                        }
-
-                        ?>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-3">
-                                <table class="table">
-                                    <thead>
-                                        <h4>
-                                            <?php echo $note ?>
-                                        </h4>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        $sql = "SELECT * from btlsv where idBTL = '$idBTL'";
-                                        $res = mysqli_query($conn, $sql);
-                                        $sn = 1;
-                                        if ($res == true) {
-                                            while ($row = mysqli_fetch_assoc($res)) {
-
-                                                $user_id = $row['user_id'];
-
-                                        ?>
-                                                <tr>
-                                                    <td>Thành viên: <?php echo $sn++ ?></td>
-                                                    <td><?php echo $row['user_id']; ?></td>
-                                                </tr>
-                                        <?php
-                                            }
-                                        }
-                                        ?>
-                                    </tbody>
-                                </table>
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <footer>
-            <p class="ftr text-center">
-                QTV - Do your best, the rest will come!
-            </p>
-        </footer>
     </div>
-    <!-- /#page-content-wrapper -->
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script>
-        $("#menu-toggle").click(function(e) {
-            e.preventDefault();
-            $("#wrapper").toggleClass("toggled");
-        });
-    </script>
 
+</div>
+<footer>
+    <p class="ftr text-center">
+        QTV - Do your best, the rest will come!
+    </p>
+</footer>
+<!-- /#page-content-wrapper -->
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+</script>
 </div>
 <!-- /#wrapper -->
