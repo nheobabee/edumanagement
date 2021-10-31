f09da181bc90f5990e1f4aff4d9dff6b370bc5f6
nheobabee <84791594+nheobabee@users.noreply.github.com>
2021-10-31 23:29:26 +0700

    tan
    css

--- admin/admin/dsnhom-btl-admin.php
+++ admin/admin/dsnhom-btl-admin.php
@@ -1,13 +1,5 @@
-<title>TEAM LIST</title>
-<?php include('../../config/config.php');
-session_start();
-if (!isset($_SESSION['loginok'])) {
-    header('location:../../login/index.php');
-}
-?>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="../../css/list-dk-btl.css">
+<!-- <link rel="stylesheet" href="../../css/list-dk-btl.css"> -->
+<?php include('./header.php') ?>
 <?php
 if (isset($_GET['idBTL'], $_GET['idMH'])) {
     $idBTL = $_GET['idBTL'];
@@ -18,122 +10,45 @@ if (isset($_GET['idBTL'], $_GET['idMH'])) {
     $nameBTL = $row1['nameBTL'];
 }
 ?>
-<div id="wrapper">
+<br>
+<a href="http://localhost/edumanagement/admin/admin/btl.php?idMH=<?php echo $idMH ?>"><button style="padding:1% 2%;" type="button" class="btn btn-secondary text-white me-2"><i class="fas fa-undo-alt"></i></button></a>
+<br><br>
+<div class="tittle-mh">
+    <h1><?php echo $nameBTL ?></h1>
+</div>
+<?php
+$sql3 = "SELECT * FROM btlsv WHERE idBTL = $idBTL";
+$sql4 = "SELECT * FROM btlsv WHERE idBTL = $idBTL";
+$res4 = mysqli_query($conn, $sql4);
+$res3 = mysqli_query($conn, $sql3);
+$row4 = mysqli_fetch_assoc($res4)
+?>
+<?php
 
-    <!-- Sidebar -->
-    <div id="sidebar-wrapper">
-        <ul class="sidebar-nav">
-            <li class="sidebar-brand">
-                <h2>STUDENT</h2>
-            </li>
-            <li>
-                <a href="index.php"><i class="fas fa-chart-line"></i> Dashboard</a>
-            </li>
-            <li>
-                <a href="teacher.php"><i class="fas fa-chalkboard-teacher"></i> Teacher</a>
-            </li>
-            <li>
-                <a href="student.php"><i class="fas fa-user-graduate"></i> Student</a>
-            </li>
-            <li>
-                <a href="subject.php"><i class="fas fa-book"></i> Subject</a>
-            </li>
-            <li>
-                <a href="learn-teach-admin.php"><i class="fas fa-school"></i> Teach - Learn</a>
-            </li>
+if ($res3 == true) {
 
-            <li>
-                <a href="result-admin.php"><i class="fas fa-poll"></i> Result</a>
-            </li>
-        </ul>
-    </div>
-    <!-- /#sidebar-wrapper -->
+?>
+    <div class="team-btl">
+        <h2 style="color:darkgrey"><?php echo $row4['note'] ?></h2>
+        <?php
+        $i = 1;
+        while ($row3 = mysqli_fetch_assoc($res3)) {
+            $user_id = $row3['user_id'];
+            $sql6 = "SELECT * FROM users WHERE user_id = $user_id";
+            $res6 = mysqli_query($conn, $sql6);
+            $row6 = mysqli_fetch_assoc($res6);
 
-    <!-- Page Content -->
-    <div id="page-content-wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
+        ?>
 
-                    <nav class="navbar navbar-light">
-                        <div class="container-fluid">
-                            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><i class="fas fa-bars"></i></a>
+            <p><span style="font-weight: 500;">Thành viên <?php echo $i ?>: </span><?php echo $row6['user_name'] ?></p>
+        <?php
+            $i++;
+        } ?>
 
-                            <form class="d-flex">
-                                <a href="" class="navbar-brand">HOME</a>
-                                <a href="" class="navbar-brand">ACCOUNT</a>
-                                <a href="../../login/logout.php" class="navbar-brand">LOGOUT</a>
-                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-success" type="submit">Search</button>
-                            </form>
-                        </div>
-                    </nav>
-                    <div class="main-content">
-
-
-                        <div class="container">
-                            <br>
-                            <a href="http://localhost/edumanagement/admin/admin/btl.php?idMH=<?php echo $idMH ?>"><button style="padding:1% 2%;" type="button" class="btn btn-secondary text-white me-2"><i class="fas fa-undo-alt"></i></button></a>
-                            <br><br>
-                            <div class="tittle-mh">
-                                <h1><?php echo $nameBTL ?></h1>
-                            </div>
-                            <?php
-                            $sql3 = "SELECT * FROM btlsv WHERE idBTL = $idBTL";
-                            $sql4 = "SELECT * FROM btlsv WHERE idBTL = $idBTL";
-                            $res4 = mysqli_query($conn, $sql4);
-                            $res3 = mysqli_query($conn, $sql3);
-                            $row4 = mysqli_fetch_assoc($res4)
-                            ?>
-                            <?php
-
-                            if ($res3 == true) {
-
-                            ?>
-                                <div class="team-btl">
-                                    <h2  style="color:darkgrey"><?php echo $row4['note'] ?></h2>
-                                    <?php
-                                    $i = 1;
-                                    while ($row3 = mysqli_fetch_assoc($res3)) {
-                                        $user_id = $row3['user_id'];
-                                        $sql6 = "SELECT * FROM users WHERE user_id = $user_id";
-                                        $res6 = mysqli_query($conn, $sql6);
-                                        $row6 = mysqli_fetch_assoc($res6);
-
-                                    ?>
-
-                                        <p><span style="font-weight: 500;">Thành viên <?php echo $i ?>: </span><?php echo $row6['user_name'] ?></p>
-                                    <?php
-                                        $i++;
-                                    } ?>
-                                    
-                                </div>
-
-                            <?php
-                            }
-                            ?>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
-</div>
-<footer>
-    <p class="ftr text-center">
-        QTV - Do your best, the rest will come!
-    </p>
-</footer>
-<!-- /#page-content-wrapper -->
-<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-</script>
-</div>
-<!-- /#wrapper -->
+<?php
+}
+?>
+
+<?php include('./footer.php') ?>
--- admin/admin/learn.php
+++ admin/admin/learn.php
@@ -1,143 +1,58 @@
-<?php
-session_start();
-?>
-
-<title>RESULT BTVN</title>
-<?php include('../../config/config.php');
-
-if (!isset($_SESSION['loginok'])) {
-    header('location:../../login/index.php');
-}
-?>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="../../css/result-admin.css">
+<?php include('./header.php') ?>
+<h1>HỌC TẬP</h1><a href="./learn-teach-admin.php"><button style="padding:1% 2%;" type="button" class="btn btn-secondary text-white me-2"><i class="fas fa-undo-alt"></i></button></a>
+<br>
 
-<div id="wrapper">
+<br>
+<a href="./add-learn-admin.php"><button type="button" class="btn btn-success text-white me-2"><i class="fas fa-plus"></i> Thêm sinh viên</button></a>
 
-    <!-- Sidebar -->
-    <div id="sidebar-wrapper">
-        <ul class="sidebar-nav">
-            <li class="sidebar-brand">
-                <h2>ADMIN</h2>
-            </li>
-            <li>
-                <a href="index.php"><i class="fas fa-chart-line"></i> Dashboard</a>
-            </li>
-            <li>
-                <a href="teacher.php"><i class="fas fa-chalkboard-teacher"></i> Teacher</a>
-            </li>
-            <li>
-                <a href="student.php"><i class="fas fa-user-graduate"></i> Student</a>
-            </li>
-            <li>
-                <a href="subject.php"><i class="fas fa-book"></i> Subject</a>
-            </li>
-            <li>
-                <a href="learn-teach-admin.php"><i class="fas fa-school"></i> Teach - Learn</a>
-            </li>
+<br><br>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">STT</th>
+            <th scope="col">Tên sinh viên</th>
+            <th scope="col">Tên môn học</th>
+            <th scope="col">Cập nhật</th>
+            <th scope="col">Xóa</th>
 
-            <li>
-                <a href="result-admin.php"><i class="fas fa-poll"></i> Result</a>
-            </li>
-        </ul>
-    </div>
-    <!-- /#sidebar-wrapper -->
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $sql = "SELECT * from relationship where note = 0";
+        $res = mysqli_query($conn, $sql);
+        $sn = 1;
+        if ($res == true) {
+            while ($row = mysqli_fetch_assoc($res)) {
+                $user_id = $row['user_id'];
+                $idMH = $row['idMH'];
 
-    <!-- Page Content -->
-    <div id="page-content-wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    <nav class="navbar navbar-light">
-                        <div class="container-fluid">
-                            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><i class="fas fa-bars"></i></a>
-
-                            <form class="d-flex">
-                                <a href="" class="navbar-brand">HOME</a>
-                                <a href="" class="navbar-brand">ACCOUNT</a>
-                                <a href="../../login/logout.php" class="navbar-brand">LOGOUT</a>
-                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-success" type="submit">Search</button>
-                            </form>
-                        </div>
-                    </nav>
-                    <div class="container">
-                        <h1>HỌC TẬP</h1><a href="./learn-teach-admin.php"><button style="padding:1% 2%;" type="button" class="btn btn-secondary text-white me-2"><i class="fas fa-undo-alt"></i></button></a>
-                        <br>
-
-                        <br>
-                        <a href="./add-learn-admin.php"><button type="button" class="btn btn-success text-white me-2"><i class="fas fa-plus"></i> Thêm sinh viên</button></a>
-
-                        <br><br>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">STT</th>
-                                    <th scope="col">Tên sinh viên</th>
-                                    <th scope="col">Tên môn học</th>
-                                    <th scope="col">Cập nhật</th>
-                                    <th scope="col">Xóa</th>
-
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $sql = "SELECT * from relationship where note = 0";
-                                $res = mysqli_query($conn, $sql);
-                                $sn = 1;
-                                if ($res == true) {
-                                    while ($row = mysqli_fetch_assoc($res)) {
-                                        $user_id = $row['user_id'];
-                                        $idMH = $row['idMH'];
-
-                                        $sql3 = "SELECT * FROM users WHERE user_id = '$user_id'";
-                                        $res3 = mysqli_query($conn, $sql3);
-                                        $row3 = mysqli_fetch_assoc($res3);
+                $sql3 = "SELECT * FROM users WHERE user_id = '$user_id'";
+                $res3 = mysqli_query($conn, $sql3);
+                $row3 = mysqli_fetch_assoc($res3);
 
 
-                                        $sql2 = "SELECT * FROM monhoc WHERE idMH = '$idMH'";
-                                        $res2 = mysqli_query($conn, $sql2);
-                                        $row2 = mysqli_fetch_assoc($res2);
+                $sql2 = "SELECT * FROM monhoc WHERE idMH = '$idMH'";
+                $res2 = mysqli_query($conn, $sql2);
+                $row2 = mysqli_fetch_assoc($res2);
 
-                                ?>
-                                        <tr>
-                                            <td><?php echo $sn++ ?></td>
-                                            <td><?php echo $row3['user_name']; ?></td>
-                                            <td><?php echo $row2['nameMH']; ?></td>
-                                            <td>
-                                                <a href="./upd-learn-admin.php?user_id=<?php echo $user_id ?>"><button type="button" class="btn btn-primary text-white me-2"><i class="fas fa-edit"></i></button></a>
-                                            </td>
-                                            <td>
-                                                <a href="./del-learn-admin.php?user_id=<?php echo $user_id ?>"><button type="button" class="btn btn-danger text-white me-2"><i class="fas fa-trash-alt"></i></button></a>
-                                            </td>
-                                        </tr>
-                                <?php
-                                    }
-                                }
-                                ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <footer>
-        <p class="ftr text-center">
-            QTV - Do your best, the rest will come!
-        </p>
-    </footer>
-</div>
-<!-- /#page-content-wrapper -->
-<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-</script>
-</div>
-<!-- /#wrapper -->
-<
+        ?>
+                <tr>
+                    <td><?php echo $sn++ ?></td>
+                    <td><?php echo $row3['user_name']; ?></td>
+                    <td><?php echo $row2['nameMH']; ?></td>
+                    <td>
+                        <a href="./upd-learn-admin.php?user_id=<?php echo $user_id ?>"><button type="button" class="btn btn-primary text-white me-2"><i class="fas fa-edit"></i></button></a>
+                    </td>
+                    <td>
+                        <a href="./del-learn-admin.php?user_id=<?php echo $user_id ?>"><button type="button" class="btn btn-danger text-white me-2"><i class="fas fa-trash-alt"></i></button></a>
+                    </td>
+                </tr>
+        <?php
+            }
+        }
+        ?>
+    </tbody>
+</table>
+<?php include('./footer.php') ?>
