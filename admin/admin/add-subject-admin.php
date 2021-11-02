5c4d40481a751bdd1e0b1af316e031a43ff45085
nheobabee <84791594+nheobabee@users.noreply.github.com>
2021-11-02 12:22:33 +0700

    TAN
    css

--- admin/admin/add-subject-admin.php
+++ admin/admin/add-subject-admin.php
@@ -2,7 +2,7 @@
 <?php include('./header.php') ?>
 <br><br>
 <div style="border-top: 5px solid lightblue;" class="add-monhoc">
-<h2>ADD SUBJECT</h2>
+<h2>THÊM MÔN HỌC</h2>
 <form method="post">
     <?php
     if (isset($_POST['add'])) {
@@ -21,15 +21,15 @@
     ?>
 
     <div class="form-group">
-        <label for="nameMH">Name:</label>
+        <label for="nameMH">Tên môn học:</label>
         <input type="text" class="form-control" id="nameMH" placeholder="Enter name" name="nameMH">
     </div>
 
     <div class="form-group">
-        <label for="TC">Credits:</label>
+        <label for="TC">Số tín chỉ:</label>
         <input type="number" value="1" class="form-control" id="TC" placeholder="Enter credits" name="TC">
     </div>
-    <button name="add" type="submit" class="btn btn-success">ADD</button>
+    <button name="add" type="submit" class="btn btn-success"><i class="fas fa-plus"></i> Thêm</button>
 </form>
 </div>
 <?php include('./footer.php') ?>
--- admin/admin/contact.php
+++ admin/admin/contact.php
@@ -1,13 +1,22 @@
 <?php include('./header.php') ?>
-<br><br>
+<br><br><br>
 <div class="all-contact">
     <div class="logo-contact">
     <i class="fas fa-university"></i>
     </div>
-    <p>QTV EDUCATION</p>
+    <div class="contact-info">
+         <h1 style="margin-bottom:10px;">QTV EDUCATION</h1>
     <p><i class="fas fa-map-marked-alt"></i> Địa chỉ: 175 Tây Sơn, Đống Đa, Hà Nội</p>
     <p><i class="fas fa-phone-alt"></i> Điện thoại: 0999999999</p>
-    <p><i class="fas fa-envelope"></i> Email: qtvedu@tlu.edu.vn</p>
+    <p><i class="fas fa-envelope"></i> Email: qtvedu@tlu.edu.vn</p> 
+    <div class="mxh">
+        <a href=""><i class="fab fa-facebook"></i></a>
+        <a href=""><i class="fab fa-instagram"></i></a>
+        <a href=""><i class="fab fa-twitter"></i></a>
+    </div>
+    </div>
+   
+   
 
 </div>
 <?php include('./footer.php') ?>
