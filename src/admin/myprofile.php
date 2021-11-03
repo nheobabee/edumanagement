20e9cf24071f694628780cab2dc867195795796d
nheobabee <84791594+nheobabee@users.noreply.github.com>
2021-11-04 00:25:23 +0700

    tan
    1

--- src/admin/myprofile.php
+++ src/admin/myprofile.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="../../css/style.css">
+<link rel="stylesheet" href="../css/style.css">
 <?php
 include('./header.php') ?>
 <form action="" method="POST" enctype="multipart/form-data">
