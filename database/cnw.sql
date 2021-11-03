6f6a84956ff195b6b408422c8b40e2113060fe54
nheobabee <84791594+nheobabee@users.noreply.github.com>
2021-11-03 23:32:36 +0700

    tan

--- database/cnw.sql
+++ database/cnw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 02, 2021 lúc 08:58 AM
+-- Thời gian đã tạo: Th10 02, 2021 lúc 05:21 PM
 -- Phiên bản máy phục vụ: 10.4.21-MariaDB
 -- Phiên bản PHP: 8.0.10
 
@@ -43,7 +43,11 @@ CREATE TABLE `btl` (
 --
 
 INSERT INTO `btl` (`idBTL`, `nameBTL`, `formatBTL`, `openedBTL`, `deadlineBTL`, `notebtl`, `filenamebtl`, `idMH`) VALUES
-(29, ' Bài tậ lớn', 'Tự luận', '2021-11-02 14:12:23', '2021-11-07 14:12:00', 'w', 'Untitled (1).png', 13);
+(33, ' Thiết kế WEB quản lý học tập', 'Tự luận', '2021-11-02 23:09:16', '2021-11-02 23:14:00', 'Nộp trước ngày thi một ngày.', 'cnw.sql', 13),
+(34, ' Thiết kế WEB quản lý khách sạn', 'Tự luận', '2021-11-02 23:09:39', '2021-11-02 23:14:00', 'Nộp trước ngày thi một ngày.', 'cnw.sql', 13),
+(35, ' Nghiên cứu hệ thống vào ra', 'Tự luận', '2021-11-02 23:13:08', '2021-11-02 16:13:00', 'Nộp trước ngày thi một ngày.', 'cnw.sql', 14),
+(36, ' Giải thuật Robinson', 'Tự luận', '2021-11-02 23:15:23', '2021-11-02 15:15:00', 'Nộp trước ngày thi một ngày.', 'cnw.sql', 18),
+(37, ' Dùng ngôn ngữ bậc cao + SQLSERVER', 'Tự luận', '2021-11-02 23:17:22', '2021-11-02 23:21:00', 'Nộp trước ngày thi một ngày.', 'cnw.sql', 19);
 
 -- --------------------------------------------------------
 
@@ -59,13 +63,6 @@ CREATE TABLE `btlsv` (
   `notebtlsv` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `ngaynop` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `btlsv`
---
-
-INSERT INTO `btlsv` (`idBTL`, `user_id`, `idMH`, `fileBTL`, `notebtlsv`, `ngaynop`) VALUES
-(29, 10, 13, '244103339_2652082735100066_6816555485889753629_n (1).png', '', '2021-11-02 14:19:38');
 
 -- --------------------------------------------------------
 
@@ -89,7 +86,14 @@ CREATE TABLE `btvn` (
 --
 
 INSERT INTO `btvn` (`idBTVN`, `nameBTVN`, `formatBTVN`, `openedBTVN`, `deadlineBTVN`, `note`, `filename`, `idMH`) VALUES
-(22, ' Dùng bootstrap', 'Tự luận', '2021-11-02 14:02:08', '2021-11-12 14:01:00', 'cố', 'chat.php', 13);
+(27, ' Dùng Bootstrap thiết kế giao diện nhà hàng', 'Tự luận', '2021-11-02 23:06:28', '2021-11-02 23:07:00', 'Làm và nộp link github.', 'cnw.sql', 13),
+(28, ' Sử dụng Javascript thiết kế form Login', 'Tự luận', '2021-11-02 23:08:26', '2021-11-02 23:10:00', 'Làm và nộp link github.', 'cnw.sql', 13),
+(29, ' Sử dụng hàng đợi.', 'Tự luận', '2021-11-02 23:11:17', '2021-11-14 23:10:00', 'Làm và nộp file PDF.', 'cnw.sql', 14),
+(30, ' Giải thuật Roun Robin', 'Tự luận', '2021-11-02 23:11:53', '2021-11-02 12:11:00', 'Làm và nộp file PDF.', 'cnw.sql', 14),
+(31, ' Bài toán Tháp Hà Nội', 'Tự luận', '2021-11-02 23:14:11', '2021-11-07 23:14:00', 'Làm và nộp file PDF.', 'cnw.sql', 18),
+(32, ' Giải thuật A*', 'Tự luận', '2021-11-02 23:14:30', '2021-11-02 12:14:00', 'Làm và nộp file PDF.', 'cnw.sql', 18),
+(33, ' Thực hành PROCEDURE', 'Tự luận', '2021-11-02 23:15:58', '2021-11-04 23:15:00', 'Làm và nộp file PDF.', 'cnw.sql', 19),
+(34, ' Thực hành FUNCTION', 'Tự luận', '2021-11-02 23:16:56', '2021-11-02 23:19:00', 'Làm và nộp file PDF.', 'cnw.sql', 19);
 
 -- --------------------------------------------------------
 
@@ -106,13 +110,6 @@ CREATE TABLE `btvnsv` (
   `ngaynop` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Đang đổ dữ liệu cho bảng `btvnsv`
---
-
-INSERT INTO `btvnsv` (`idBTVN`, `user_id`, `fileBTVN`, `notebtvnsv`, `idMH`, `ngaynop`) VALUES
-(22, 10, '244103339_2652082735100066_6816555485889753629_n.png', 'em nộp', 13, '2021-11-02 14:03:31');
-
 -- --------------------------------------------------------
 
 --
@@ -125,13 +122,6 @@ CREATE TABLE `dkbtl` (
   `idMH` int(11) DEFAULT NULL,
   `note` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `dkbtl`
---
-
-INSERT INTO `dkbtl` (`user_id`, `idBTL`, `idMH`, `note`) VALUES
-(10, 29, 13, '');
 
 -- --------------------------------------------------------
 
@@ -158,13 +148,6 @@ CREATE TABLE `ketquabtvn` (
   `markBTVN` int(11) NOT NULL,
   `cmtBTVN` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `ketquabtvn`
---
-
-INSERT INTO `ketquabtvn` (`user_id`, `idBTVN`, `markBTVN`, `cmtBTVN`) VALUES
-(10, 22, 10, 'tốt');
 
 -- --------------------------------------------------------
 
@@ -206,13 +189,34 @@ CREATE TABLE `relationship` (
 
 INSERT INTO `relationship` (`user_id`, `idMH`, `note`) VALUES
 (10, 13, 0),
+(10, 14, 0),
+(10, 18, 0),
+(10, 19, 0),
 (11, 13, 0),
-(12, 13, 1),
-(12, 14, 1),
 (13, 13, 0),
-(15, 19, 1),
-(20, 18, 1),
-(37, 13, 1);
+(14, 13, 0),
+(14, 14, 0),
+(14, 19, 0),
+(16, 14, 0),
+(17, 14, 0),
+(18, 14, 0),
+(18, 18, 0),
+(19, 14, 0),
+(21, 18, 0),
+(22, 18, 0),
+(22, 19, 0),
+(23, 18, 0),
+(24, 13, 0),
+(24, 18, 0),
+(25, 19, 0),
+(26, 19, 0),
+(27, 19, 0),
+(28, 19, 0),
+(30, 18, 0),
+(44, 18, 1),
+(45, 13, 1),
+(46, 19, 1),
+(47, 14, 1);
 
 -- --------------------------------------------------------
 
@@ -233,16 +237,9 @@ CREATE TABLE `tbl_comment` (
 --
 
 INSERT INTO `tbl_comment` (`comment_id`, `parent_comment_id`, `comment`, `comment_sender_name`, `date`) VALUES
-(20, 0, '1', '1', '2021-11-02 06:42:43'),
-(21, 0, 'hihi', 'quân', '2021-11-02 06:42:59'),
-(22, 0, 'vương đây', 'Vương', '2021-11-02 06:43:43'),
-(23, 0, 'comment', 'Nguyễn Minh Vương', '2021-11-02 06:59:25'),
-(24, 23, 'v', 'Nguyễn Minh Vương', '2021-11-02 06:59:42'),
-(25, 0, 'hi', 'Hồ Hữu Thiếng', '2021-11-02 07:00:16'),
-(26, 25, 'hi', 'Hồ Hữu Thiếng', '2021-11-02 07:00:19'),
-(27, 23, 'hi', 'Hồ Hữu Thiếng', '2021-11-02 07:00:24'),
-(28, 0, 'quânqueo', 'Hồ Hồng Quân', '2021-11-02 07:46:42'),
-(29, 25, 'hi', 'Hồ Hồng Quân', '2021-11-02 07:46:46');
+(44, 0, 'hi', 'ADMIN', '2021-11-02 10:43:46'),
+(45, 44, 's', 'ADMIN', '2021-11-02 10:43:51'),
+(46, 0, 'n', 'Hồ Hữu Thiếng', '2021-11-02 13:06:54');
 
 -- --------------------------------------------------------
 
@@ -270,18 +267,14 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `user_name`, `user_avatar`, `user_gioitinh`, `user_birthday`, `user_phone`, `user_email`, `user_pass`, `user_level`, `status`, `code`, `code_forgot`) VALUES
-(3, 'nheo', '137710-1068x543.jpg', 0, '2021-10-27', '0386653766', 'tannguyentttbs000@gmail.com', '$2y$10$YAqIoqhgwjs9W3kqYm1pYOwMTEBRhEraXPdlfpiFqoNJpOEBjmbMS', 2, 1, '6db1e92aa9c85c06baf66f5557769f22', ''),
-(10, 'Hồ Hồng Quân', ' img/kakashi.png', 1, '2001-11-25', '0346785893', 'aqdz01@gmail.com', '$2y$10$TySByXD7JoufRz9qAoOes.o57J0r5LqV6tiiry4GE9bnDekiZgtLW', 0, 1, '01d5adc62fdbba0c64eae55eddb8bbb5', ''),
+(10, 'Hồ Hồng Quân', ' img/kakashi.png', 1, '2001-11-25', '0346785893', 'hhq@gmail.com', '$2y$10$TySByXD7JoufRz9qAoOes.o57J0r5LqV6tiiry4GE9bnDekiZgtLW', 0, 1, '01d5adc62fdbba0c64eae55eddb8bbb5', ''),
 (11, 'Hồ Đức Hiếu', ' img/46508791_495542080957835_6194235171827351552_o.jpg', 1, '2005-01-30', '0335785981', 'anhqh25@gmail.com', '$2y$10$dPQCRa.NBG4OmhLxvFRe/uXGm6BYdErM6RqWSgoQpUdNIOTk7nDSm', 0, 1, 'dcda77b53070092589cc299187ea67aa', ''),
-(12, 'Hồ Hữu Thiếng', ' img/9bad078b80762dfbc12ec227e9acc505.jpg', 1, '1970-07-04', '0976156033', 'huuthieng70@gmail.com', '$2y$10$g51N74jgfuACLvzVpAxNYu66BIqLTF8wTrMNvhiGB6CdZWPWYBwcC', 1, 1, 'd65c2ac6b939451291b77c1541014171', ''),
 (13, 'Nguyễn Thị Bích Loan', ' img/137321599_208840064293477_7185830937899760390_n.jpg', 0, '2021-11-26', '0386653766', 'nheokid@gmail.com', '$2y$10$wZlqKKDY7rAAmtzAPsZaNedHj5YD/dcySGThysA5b6J.unBU1VKgO', 0, 1, 'a21fd45a9709a054ba2dc85e69231162', ''),
 (14, 'Lê Thị Thu Hiền', ' img/137321599_208840064293477_7185830937899760390_n.jpg', 0, '2001-01-28', '0988784753', 'nheokids@gmail.com', '$2y$10$HHlVqz2M5YsXV7kypEiFBuBgBQQwJjatm/oDMgNPkrDzRBW1udvfS', 0, 1, 'f5ff2b48c11b83635d4d310527026aee', ''),
-(15, 'Nguyễn Văn Thức', ' img/137321599_208840064293477_7185830937899760390_n.jpg', 1, '2001-04-06', '0994885768', 'tannguyentttbss@gmail.com', '$2y$10$eNve5X3EEnEL.1eW9qwN6.VZ7Ldc3OULFtKH1Ei/vStcRFf0WUz0e', 1, 1, '77ca66dd4815d6c169b679c966b9ad22', ''),
 (16, 'Phạm Thị Thúy', ' img/137321599_208840064293477_7185830937899760390_n.jpg', 0, '2001-04-06', '0998887665', 'nheo.sosweet@gmail.com', '$2y$10$a3AQ7KdJ8NTrezXtArgUqu/ubxD5UQgVRtOR/00nkArwHk3xgrBmS', 0, 1, '4f615b8cd9a7bc8fc45dafa0344652f7', ''),
 (17, 'Vũ Thị Nguyệt ', ' img/1_N86mYdIydgljjYnn8xwJgw.jpeg', 1, '1974-11-20', '0382373892', 'vuthinguyet1974@gmail.com', '$2y$10$dYkIcLHW6stis8Vb8okgDey9RBOSGiCbFO/8m55AEhScNYqmUsfe2', 0, 1, '3b2883c0487c64d70967a75ee7ab0fbc', ''),
 (18, 'Đinh Trọng Thi', ' img/212-1068x601.jpg', 1, '2001-01-06', '0392871853', 'anhthi06@gmail.com', '$2y$10$1b0HOeM38mo61YLAJXChdu.UL38UFxMuWjoR8JZ8DbheXmZ97GOJ2', 0, 1, '833682d42549e01eddff86c9a6d76dda', ''),
 (19, 'Hồ Khắc Hoàng', ' img/235282.jpg', 1, '2001-07-14', '0435244205', 'sv1@gmail.com', '$2y$10$Qm4uVNwms0d2PTQVACRn/uXTC4Go80gnj2ce4SqoIdyFaw/IyS3uW', 0, 1, 'a8eeed4db892e21396ca396aa862c3df', ''),
-(20, 'Nguyễn Minh Vương', ' img/123832942_1335973143449028_478660641033546830_n.jpg', 1, '2001-07-25', '0342443492', 'vuong9xx@gmail.com', '$2y$10$8XmHxkZMG6fBNjhU7sSxnuBcQ4ZffPUF8lY8a34nvewkfJkzJKwca', 1, 1, '7c24b0cfd14b0a5cbaeb6bb7f10dde21', ''),
 (21, 'Trịnh Hoàng Long', ' img/ad.jpg', 1, '2001-10-29', '0424234234', 'sv2@gmail.com', '$2y$10$YmL9EnhWUgH.vVC8L0NWuOLqsCIu9KCk9MNW7l8D/Y/kQBHAZRljK', 0, 1, '502b975067dd1bfcd70be9ff0f1701c9', ''),
 (22, 'Nguyễn Minh Đức', ' img/65569960_p0-1068x1068.jpg', 1, '2001-09-20', '0334543824', 'sv3@gmail.com', '$2y$10$cXIUKbEk9Y4/gzMo75x1e.2lcp4QildGrJKHu2jeTJ89f9yF5ROX.', 0, 1, 'aaf181883cfda88034065d375f5ff02d', ''),
 (23, 'Tô Duy Đức', ' img/212-1068x601.jpg', 1, '2001-12-03', '034234832', 'sv4@gmail.com', '$2y$10$lja.VjZLfCEQJ7bH8geVKOS6B6HHalmYt64KyK9oFwUaM67smICNu', 0, 1, '74a3910be3e3aba1285d9ebb8860ecb8', ''),
@@ -298,7 +291,11 @@ INSERT INTO `users` (`user_id`, `user_name`, `user_avatar`, `user_gioitinh`, `us
 (34, 'Nguyễn Lan Anh', ' img/242768425_294331492154723_1251493049399012932_n.jpg', 1, '2001-06-13', '0434823489', 'sv15@gmail.com', '$2y$10$hfa9DpmYKqg35tbFRjKtL.YRAC.QKnFtHuV0N7JpXSODGRPqyLQfe', 0, 1, '82474e3a38ce703929e577bd7313f78d', ''),
 (35, 'Trần Quỳnh Anh', ' img/244103339_2652082735100066_6816555485889753629_n.png', 1, '2001-09-02', '038428349', 'sv16@gmail.com', '$2y$10$4EtBHJ3wXH4mlcTmgaM5r.dkbRonm70amwrGMESIHmnGrXm3fG.EC', 0, 1, '43689b3b23dcad85e81ee7033d6e5c87', ''),
 (36, 'Nguyễn Phương Linh', ' img/f1ab5986755d71f252d8839214643246.jpg', 0, '2001-10-02', '034293842', 'sv17@gmail.com', '$2y$10$z3lDiSbB7uTudLDbFnAgKufqoEUdGzvTrGd2blO5alPhjhDqxB5M.', 0, 1, '09886dfa8ae83f6639e8d0024a36239b', ''),
-(37, 'Kiều Tuấn Dzũng', ' img/245497736_1502927523395536_273617092518582484_n (2).jpg', 1, '1990-11-26', '088888888', 'nheo.4fun@gmail.com', '$2y$10$H//UVbHqCLd6Il1h9OeP5euS4agZMwU9sYGud98zjOf8o5x0K278i', 1, 1, '92c17c792a2ec0ea9a38688e612a3a7e', '');
+(39, 'ADMIN', ' img/unknown1.jpg', 1, '2021-11-03', '113', 'admin@gmail.com', '$2y$10$xIo/DAAQuqyoZ4q/WRvzze4vyIby.kJgj9JHrgHCvVlIhgeMAVBW2', 2, 1, '525b05b26b5477b5af0d478647bcc5b5', ''),
+(44, 'Trần Thị Cẩm Giang', ' img/137321599_208840064293477_7185830937899760390_n.jpg', 0, '1990-12-12', '099999999', 'ai@gmail.com', '$2y$10$o4NchcDwZGlCyrFstIYieejp.xysjWaXidOOBxsOzHE03FB0LrfOy', 1, 1, '23163b29a2634d4864d4fa10766d539e', ''),
+(45, 'Kiều Tuấn Dzũng', ' img/137321599_208840064293477_7185830937899760390_n.jpg', 1, '1989-12-09', '088888888', 'cnw@gmail.com', '$2y$10$Qpd.E8VrU2OklDm6Y2oaiODehVHC.LIGRhx0rUAeJKTjyoYdVcXmO', 1, 1, 'a3a947d3388f367fae6c6625e6f22ec1', ''),
+(46, 'Nguyễn Ngọc Quỳnh Châu', ' img/137321599_208840064293477_7185830937899760390_n.jpg', 0, '1991-07-09', '0777777777', 'csdl@gmail.com', '$2y$10$WqUSiYta/HEDjnb/.PchP.5AVavG3HQ1u/UvQqq.RrN7mrltYmilC', 1, 1, 'd9f0a46cbbddcc289296ccf7707e16fc', ''),
+(47, 'Đoàn Thị Quế', ' img/137321599_208840064293477_7185830937899760390_n.jpg', 0, '1988-12-09', '0666666666', 'hdh@gmail.com', '$2y$10$cOIrRycZPt.aY7epHku5putzMsUSQJQ/b8psTuSTFHXU6kPnV.yem', 1, 1, '91a13da344d2ba0a5d60e738db9e0dc4', '');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -389,31 +386,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT cho bảng `btl`
 --
 ALTER TABLE `btl`
-  MODIFY `idBTL` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `idBTL` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT cho bảng `btvn`
 --
 ALTER TABLE `btvn`
-  MODIFY `idBTVN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `idBTVN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT cho bảng `monhoc`
 --
 ALTER TABLE `monhoc`
-  MODIFY `idMH` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `idMH` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT cho bảng `tbl_comment`
 --
 ALTER TABLE `tbl_comment`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
